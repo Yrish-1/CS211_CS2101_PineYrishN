f2ca66f8e6c49360a0fe3de02af97d157e9d1f9b
PineYrish <pineyrish@gmail.com>
2025-09-04 14:19:34 +0800

    Fibonacci.java

---
+++ 01 - Practice Assessments/Practice Assessment 1/Fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        
+        
+        int n = scanner.nextInt();
+        int first = 0, second = 1;
+
+        System.out.println("Fibonacci Sequence up to " + n + " terms: );
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");//magssilbing loop hanggang makuha yung inenter na number 
+
+            int next = first + second;//formula ata ito hehe (to study)
+            first = second;
+            second = next;
+        }
+
+        scanner.close();
+    }
+}
