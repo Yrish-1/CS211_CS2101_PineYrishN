610e6b11a58bafc9387e961a1f995d6842f7f6a8
PineYrish <pineyrish@gmail.com>
2025-09-04 14:04:39 +0800

    SuperDigit.java

---
+++ 01 - Practice Assessments/Practice Assessment 1/SuperDigit.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class SuperDigit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer up to 18 digits: ");
+        String num = input.nextLine();
+
+        String Orig = num;  
+        while (true) {
+           
+            if (num.length() == 1) {
+                break;
+            }
+
+            int sum = 0;
+
+            for (int y = 0; y < num.length(); y++) {
+                char ch = num.charAt(y);
+
+                if (ch >= '0' && ch <= '9') {
+                    sum += ch - '0';
+                }
+            }
+
+            num = String.valueOf(sum);
+        }
+
+        System.out.println("Original number: " + Orig);
+        System.out.println("Super digit: " + num);
+    }
+}
