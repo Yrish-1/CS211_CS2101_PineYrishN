04e6a4a2af134c334be37b1d21e4aab4671f0b6a
PineYrish <pineyrish@gmail.com>
2025-09-07 22:55:55 +0800

    Update SuperDigit.java

--- 01 - Practice Assessments/Practice Assessment 1/SuperDigit.java
+++ 01 - Practice Assessments/Practice Assessment 1/SuperDigit.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
 public class SuperDigit {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);//allows the program to read what the user is prompting
 
         System.out.print("Enter an integer up to 18 digits: ");
         String num = input.nextLine();
 
         String Orig = num;  
-        while (true) {
+        while (true) { 
            
             if (num.length() == 1) {
-                break;
+                break;//kapag single number lang break agad kasi the number itself is its superdigit
             }
 
             int sum = 0;
 
             for (int y = 0; y < num.length(); y++) {
-                char ch = num.charAt(y);
-
+                char ch = num.charAt(y);//mag loloop through sa ininput na number 
                 if (ch >= '0' && ch <= '9') {
-                    sum += ch - '0';
+                    sum += ch - '0'; //kapag '0' ay 48 and so on (//4713 = 4 52-48 = 4)
                 }
             }
 
