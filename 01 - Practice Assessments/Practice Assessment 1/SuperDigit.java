5004a5279fc395d4a7fa3e72cae9630c480c628c
Yrish-1 <pineyrish@gmail.com>
2025-10-28 10:22:01 +0800

    new

--- 01 - Practice Assessments/Practice Assessment 1/SuperDigit.java
+++ 01 - Practice Assessments/Practice Assessment 1/SuperDigit.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
+
 public class SuperDigit {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);//allows the program to read what the user is prompting
+        Scanner value = new Scanner(System.in);// allows the program to read what the user is prompting
 
         System.out.print("Enter an integer up to 18 digits: ");
-        String num = input.nextLine();
+        String num = value.nextLine();
 
-        String Orig = num;  
-        while (true) { 
-           
+        String Orig = num;
+        while (true) {
+
             if (num.length() == 1) {
-                break;//kapag single number lang break agad kasi the number itself is its superdigit
+                break;// kapag single number lang break agad kasi the number itself is its superdigit
             }
 
             int sum = 0;
 
             for (int y = 0; y < num.length(); y++) {
-                char ch = num.charAt(y);//mag loloop through sa ininput na number 
+                char ch = num.charAt(y);// mag loloop through sa ininput na number
                 if (ch >= '0' && ch <= '9') {
-                    sum += ch - '0'; //kapag '0' ay 48 and so on (//4713 = 4 52-48 = 4)
+                    sum += ch - '0'; // kapag '0' ay 48 and so on (//4713 = 4 52-48 = 4)
                 }
             }
 
@@ -26,5 +27,7 @@ public class SuperDigit {
         }
 
         System.out.println("Super Digit of " + Orig + ": " + num);
+
+        value.close();
     }
 }
--- 01 - Practice Assessments/Practice Assessment 3/GradeCalculator.java
+++ 01 - Practice Assessments/Practice Assessment 3/GradeCalculator.java
@@ -1,62 +1,62 @@
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String args[]){
+public class GradeCalculator {
+    public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
-        
+
         System.out.print("Enter number of grades: ");
         int n = input.nextInt();
-        
+
         System.out.print("Enter grades: ");
         int[] grades = new int[n];
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             grades[i] = input.nextInt();
         }
-        
+
         System.out.println();
-        
+
         int[] passing = new int[100];
         int[] failing = new int[100];
         int passingG = 0;
         int failingG = 0;
-        
-        for(int i = 0; i < n; i++){
+
+        for (int i = 0; i < n; i++) {
             int grade = grades[i];
             int adjustedG = grade;
-            
-            if(grade >= 78){
+
+            if (grade >= 78) {
                 int multiple = (grade / 5) * 5;
                 int diff = multiple - grade;
-                
-                if(diff < 3){
+
+                if (diff < 3) {
                     adjustedG = multiple;
                 }
             }
-            if (adjustedG >= 80){
+            if (adjustedG >= 80) {
                 passing[passingG] = adjustedG;
                 passingG++;
-            }else{
+            } else {
                 failing[failingG] = adjustedG;
                 failingG++;
             }
         }
-        
-        if(passingG > 0){
+
+        if (passingG > 0) {
             System.out.print("Passed: ");
-            for(int i = 0; i < passingG; i++){
+            for (int i = 0; i < passingG; i++) {
                 System.out.print(passing[i]);
-                if(i < passingG - 1){
+                if (i < passingG - 1) {
                     System.out.print(" ");
                 }
             }
             System.out.println();
         }
-        
-        if(failingG > 0){
+
+        if (failingG > 0) {
             System.out.print("Failed: ");
-            for(int i = 0; i < failingG; i++){
+            for (int i = 0; i < failingG; i++) {
                 System.out.print(failing[i]);
-                if(i < failingG - 1){
+                if (i < failingG - 1) {
                     System.out.print(" ");
                 }
             }
@@ -64,11 +64,11 @@ public class Main{
         }
         int stellarStar = passingG / 3;
         int tonitrusBolt = failingG;
-        
+
         System.out.println();
-        System.out.println("Stella Stars: "+ stellarStar);
+        System.out.println("Stella Stars: " + stellarStar);
         System.out.println("Tonitrus Bolts: " + tonitrusBolt);
-        
+
         input.close();
     }
 }
